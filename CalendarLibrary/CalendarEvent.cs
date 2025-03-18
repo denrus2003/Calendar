a52aa932d54dfdfdb93085ecd1400448e188ddfe
Денис Солдатов <denverrus2003@gmail.com>
2025-03-18 13:19:11 +0300

    Библиотека классов Calendar Library

---
+++ CalendarLibrary/CalendarEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CalendarLibrary
+{
+    public class CalendarEvent
+    {
+        public DateTime Date { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public CalendarEvent(DateTime date, string title, string description)
+        {
+            Date = date;
+            Title = title;
+            Description = description;
+        }
+
+        public override string ToString()
+        {
+            return $"{Date.ToShortDateString()} - {Title}";
+        }
+    }
+}
