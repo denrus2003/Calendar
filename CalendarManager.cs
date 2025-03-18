200bad24e4ac1fd564c05c58afc9c9a0ecf5b087
Денис Солдатов <denverrus2003@gmail.com>
2025-03-18 13:06:27 +0300

    Добавьте файлы проекта.

---
+++ CalendarManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace CalendarLibrary
+{
+    public class CalendarManager
+    {
+        private List<CalendarEvent> _events;
+
+        public CalendarManager()
+        {
+            _events = new List<CalendarEvent>();
+        }
+
+        public void AddEvent(CalendarEvent calendarEvent)
+        {
+            _events.Add(calendarEvent);
+        }
+
+        public bool RemoveEvent(CalendarEvent calendarEvent)
+        {
+            return _events.Remove(calendarEvent);
+        }
+
+        public IEnumerable<CalendarEvent> GetEvents()
+        {
+            return _events;
+        }
+    }
+}
