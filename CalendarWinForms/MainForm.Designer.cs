784afb83ab7556c93b6327126bfa09d25af588cf
Денис Солдатов <denverrus2003@gmail.com>
2025-03-19 11:04:05 +0300

    Приложение WinForms

---
+++ CalendarWinForms/MainForm.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace CalendarWinForms
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.ListBox listBoxEvents;
+        private System.Windows.Forms.Button btnAddEvent;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.listBoxEvents = new System.Windows.Forms.ListBox();
+            this.btnAddEvent = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxEvents
+            // 
+            this.listBoxEvents.FormattingEnabled = true;
+            this.listBoxEvents.ItemHeight = 16;
+            this.listBoxEvents.Location = new System.Drawing.Point(12, 12);
+            this.listBoxEvents.Name = "listBoxEvents";
+            this.listBoxEvents.Size = new System.Drawing.Size(360, 180);
+            this.listBoxEvents.TabIndex = 0;
+            // 
+            // btnAddEvent
+            // 
+            this.btnAddEvent.Location = new System.Drawing.Point(12, 210);
+            this.btnAddEvent.Name = "btnAddEvent";
+            this.btnAddEvent.Size = new System.Drawing.Size(150, 30);
+            this.btnAddEvent.TabIndex = 1;
+            this.btnAddEvent.Text = "Добавить событие";
+            this.btnAddEvent.UseVisualStyleBackColor = true;
+            this.btnAddEvent.Click += new System.EventHandler(this.btnAddEvent_Click);
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.btnAddEvent);
+            this.Controls.Add(this.listBoxEvents);
+            this.Name = "MainForm";
+            this.Text = "Календарь WinForms";
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ CalendarWinForms/MainForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+using CalendarLibrary;
+
+namespace CalendarWinForms
+{
+    public partial class MainForm : Form
+    {
+        private CalendarManager calendar;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            calendar = new CalendarManager();
+            RefreshEvents();
+        }
+
+        private void btnAddEvent_Click(object sender, EventArgs e)
+        {
+            // В этом примере для простоты события добавляются с текущей датой и фиксированными данными.
+            DateTime date = DateTime.Now;
+            string title = "Новое событие";
+            string description = "Описание события";
+
+            calendar.AddEvent(new CalendarEvent(date, title, description));
+            RefreshEvents();
+        }
+
+        private void RefreshEvents()
+        {
+            // Обновляем источник данных для ListBox.
+            listBoxEvents.DataSource = null;
+            listBoxEvents.DataSource = new BindingSource { DataSource = calendar.GetEvents() };
+        }
+    }
+}
